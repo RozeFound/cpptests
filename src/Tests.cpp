080e5ce47df1b21acf6bccef3780bbb50e094315
RozeFound <rozefound@gmail.com>
2022-06-14 19:08:58 +0700

    Restructuring part 2

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -13,6 +13,11 @@
 #include <functional>
 #include <algorithm>
 
+
+#include <xxh3.h>
+#include "sha1.hpp"
+#include "sha512.hpp"
+
 namespace u = RozeFoundUtils;
 
 bool is_prime(int n) {
@@ -77,4 +82,32 @@ void ArrayTest() {
 	std::ranges::sort(arr);
 
 	u::print("Array:", arr | ext::join);
+}
+
+uint32_t MurmurHash2 (const void * key, int len, uint32_t seed);
+
+void test_hashes() {
+	if (auto file = u::read_from_file("/home/rozefound/Desktop/Sodium.jar")) {
+
+		u::makeTimer("xxhash",  [&]{
+			auto hash = XXH3_64bits(file->data(), file->size());
+		});
+
+		u::makeTimer("sha1", [&]{
+			SHA1 checksum;
+			checksum.update(*file);
+			auto hash = checksum.final();
+		});
+
+		u::makeTimer("sha512", [&]{
+			auto hash = sw::sha512::calculate(*file);
+		});
+
+		u::makeTimer("murmur2", [&]{
+			auto data = *file;
+			for (char c: {9, 10, 13, 32})
+				data.erase(std::remove(data.begin(), data.end(), c), data.end());
+			auto hash = MurmurHash2(data.data(), data.size(), 1);
+		});
+	};
 }
---
+++ src/Tests.hpp
@@ -0,0 +1,4 @@
+bool is_prime(int n);
+void MultiThreading();
+void ArrayTest();
+void test_hashes();
